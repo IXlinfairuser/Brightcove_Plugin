9ef3fc0d0ffa9ef4be5a75adfb2dfcbdc101c44d
IXlinfairuser <IXlinfairuser@gmail.com>
2017-06-14 14:51:44 +0800

    Create plugin.js

--- stillWatching/plugin.js
+++ stillWatching/plugin.js
@@ -2,7 +2,7 @@ videojs.plugin('stillWatching', function () {
     var player = this;
     var timeCheckPoint = 0;
     player.on('timeupdate', function () {
-        if (player.currentTime() - timeCheckPoint >= 60) {
+        if (player.currentTime() - timeCheckPoint >= 1800) {
             player.pause();
             timeCheckPoint = player.currentTime();
             player.overlay({
@@ -10,7 +10,7 @@ videojs.plugin('stillWatching', function () {
                     start: timeCheckPoint,
                     end: 'playing',
                     align: 'top',
-                    content: " 已連續播放1分鐘 , 休息一下吧"
+                    content: " 已連續播放30分鐘 , 休息一下吧"
                 }]
             });
             var overlayContainer = document.getElementsByClassName('vjs-overlay')[0];
