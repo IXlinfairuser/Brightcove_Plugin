2ce2959d24c5252c4c782b8140e0e4469c74efed
indianstau <indianstau@yahoo.com.tw>
2017-08-23 17:41:02 +0800

    adStop

---
+++ adCountdownTimer/adstop.js
@@ -0,0 +1,24 @@
+videojs.plugin('adCountDownTimer', function(){
+	var player = this;
+	var timeRemaining;
+	var countdownTimer;
+	player.on('ads-ad-started', function(){
+		document.getElementById('ad-overlay').style.fontSize = '1.2em';
+		document.getElementById('ad-overlay').style.color = 'white';
+		document.getElementById('ad-overlay').style.zIndex = 999;
+		document.getElementById('ad-overlay').style.fontWeight = 600;
+		countdownTimer = setInterval(function (){
+			timeRemaining = player.ima3.adsManager.getRemainingTime();
+			document.getElementById('timeRemaining').innerHTML = Math.round(timeRemaining);
+		}, 300);
+	});
+	player.on('ads-ad-ended', function(){
+		clearInterval(countdownTimer);
+        if(player.paused) {
+            player.play();
+        }
+	});
+	player.one('playing', function(){
+		clearInterval(countdownTimer);
+	});
+})
